73347414c3fd06b837afa4935f7d4b83905ab256
pquiros <pquiros00@gmail.com>
2018-11-30 17:27:24 -0600

    Quitar php, foto y botones. Detalles menores

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "images/";//"uploads/"
+/*$target_dir = "images/";//"uploads/"
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -27,7 +27,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png"/* && $imageFileType != "jpeg"
 && $imageFileType != "gif" */) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    /*echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -40,5 +40,5 @@ if ($uploadOk == 0) {
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
-}
+}*/
 ?>
